8d5ea9714573a99bae50e70fb336e84fd013d50c
William Oldham <wm.oldham@gmail.com>
2022-03-22 09:33:08 -0400

    update approach to rendering manuscript and supplement

--- inst/templates/_targets.R
+++ inst/templates/_targets.R
@@ -34,15 +34,67 @@ list(
 
   # write manuscript --------------------------------------------------------
 
+  # tar_target(
+  #   template,
+  #   system.file("manuscript/template.docx", package = "McGarrity.2022.hypoxia.omics"),
+  #   format = "file"
+  # ),
+  # tar_target(
+  #   pkgs,
+  #   system.file("manuscript/packages.bib", package = "McGarrity.2022.hypoxia.omics"),
+  #   format = "file"
+  # ),
+  # tar_target(
+  #   bib,
+  #   system.file("manuscript/library.json", package = "McGarrity.2022.hypoxia.omics"),
+  #   format = "file"
+  # ),
+  # tar_target(
+  #   csl,
+  #   system.file("manuscript/cell-metabolism.csl", package = "McGarrity.2022.hypoxia.omics"),
+  #   format = "file"
+  # ),
   # tar_render(
   #   manuscript,
   #   path = path_to_manuscript("manuscript.Rmd"),
-  #   output_dir = path_to_manuscript("")
+  #   output_dir = path_to_manuscript(""),
+  #   output_format = bookdown::word_document2(
+  #     reference_docx = template,
+  #     df_print = "kable",
+  #     fig_caption = TRUE,
+  #     number_sections = FALSE,
+  #     pandoc_args = c(
+  #       "--lua-filter=scholarly-metadata.lua",
+  #       "--lua-filter=author-info-blocks.lua",
+  #       "--lua-filter=pagebreak.lua"
+  #     )
+  #   ),
+  #   params = list(
+  #     bibliography = c(bib, pkgs),
+  #     csl = csl
+  #   )
   # ),
   # tar_render(
   #   supplement,
   #   path = path_to_manuscript("supplement.Rmd"),
-  #   output_dir = path_to_manuscript("")
+  #   output_dir = path_to_manuscript(""),
+  #   output_format = bookdown::word_document2(
+  #     reference_docx = template,
+  #     df_print = "kable",
+  #     fig_caption = TRUE,
+  #     number_sections = FALSE,
+  #     pandoc_args = c(
+  #       "--lua-filter=scholarly-metadata.lua",
+  #       "--lua-filter=author-info-blocks.lua",
+  #       "--lua-filter=pagebreak.lua",
+  #       "--lua-filter=multiple-bibliographies.lua"
+  #     )
+  #   ),
+  #   params = list(
+  #     bibliography = c(bib, pkgs),
+  #     csl = csl
+  #   )
   # ),
+
   NULL
 )
